74598b92d35fd5c0e0ff2d3788e8f53358014995
tyVespA <migliavacca.marco08@gmail.com>
2024-10-24 20:07:09 +0200

    Finish 2.9
    need to fix async

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
+import ShowPeople from "./components/ShowPeople";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123123123" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [filteredNames, setFilteredNames] = useState([]);
+
+  function filterNames() {
+    setFilteredNames(
+      persons.filter((person) => {
+        return person.name.toLowerCase().includes(filter.toLowerCase());
+      })
+    );
+  }
 
   function addName(e) {
     e.preventDefault();
@@ -22,24 +37,41 @@ const App = () => {
         // break;
       }
     }
-    if (nameExists == false) {
+    if (!nameExists) {
       setPersons(persons.concat(personObject));
       setNewName("");
       setNewNumber("");
     }
-  }
 
+    filterNames();
+  }
   function handleNameChange(e) {
     setNewName(e.target.value);
+    filterNames();
   }
 
   function handleNumberChange(e) {
     setNewNumber(e.target.value);
+    filterNames();
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with{" "}
+          <input
+            value={filter}
+            onChange={(e) => {
+              setFilter(e.target.value);
+              filterNames();
+              console.log("filtered names:" + JSON.stringify(filteredNames));
+            }}
+          />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -52,15 +84,19 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person) => (
-          <p key={person.name}>
-            {person.name} {person.number}
-          </p>
-        ))}
-      </div>
+
+      <ShowPeople
+        persons={persons}
+        filter={filter}
+        filteredNames={filteredNames}
+      />
+      {/* 
+      {filteredNames.map((person) => (
+        <p key={person.id}>
+          {person.name} {person.number}
+        </p>
+      ))} */}
     </div>
   );
 };
-
 export default App;
