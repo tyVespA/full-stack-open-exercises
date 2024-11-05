686abadb574b1fa9db9f0a63c01c05a37a1b30d8
tyVespA <migliavacca.marco08@gmail.com>
2024-11-05 12:36:49 +0100

    2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,21 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  function addNewPerson(e) {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    let updatedPersons = [...persons, newPerson];
+    setPersons(updatedPersons);
+    setNewName("");
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input />
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>debug: {newName}</div>
         <div>
@@ -17,7 +26,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
