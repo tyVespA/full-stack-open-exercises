f9d791b02e487a675deb5176f5f033dacb299cee
tyVespA <migliavacca.marco08@gmail.com>
2024-11-05 12:54:11 +0100

    2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,27 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  function checkDuplicate(newName, persons) {
+    for (let person of persons) {
+      if (newName == person.name) {
+        alert(`${newName} is already added to phonebook`);
+        return false;
+      }
+    }
+    return true;
+  }
+
   function addNewPerson(e) {
     e.preventDefault();
     const newPerson = { name: newName };
-    let updatedPersons = [...persons, newPerson];
-    setPersons(updatedPersons);
-    setNewName("");
+
+    if (checkDuplicate(newName, persons)) {
+      let updatedPersons = [...persons, newPerson];
+      setPersons(updatedPersons);
+      setNewName("");
+    } else {
+      setNewName("");
+    }
   }
 
   return (
