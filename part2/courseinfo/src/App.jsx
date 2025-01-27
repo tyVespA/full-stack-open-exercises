adcc9ce4a0282232c8cf1a5a5864acc7ea622bee
tyVespA <migliavacca.marco08@gmail.com>
2025-01-27 14:37:49 +0100

    add title

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -48,6 +48,7 @@ const App = () => {
 
   return (
     <div>
+      <h1>Web Development Curriculum</h1>
       {courses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
