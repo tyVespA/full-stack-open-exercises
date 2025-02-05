2be35ff111de72d4a8556f99f62e73effd6606f8
tyVespA <migliavacca.marco08@gmail.com>
2025-02-05 11:54:02 +0100

    complete 4.4

--- part4/tests/listtHelper.test.js
+++ part4/tests/listtHelper.test.js
@@ -8,3 +8,59 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithMoreBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 1,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 4,
+      __v: 0,
+    },
+  ];
+
+  const listWithNoBlogs = [];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("when list has more blogs, equals the sum of likes", () => {
+    const result = listHelper.totalLikes(listWithMoreBlogs);
+    assert.strictEqual(result, 10);
+  });
+  test("when list has no, equals 0", () => {
+    const result = listHelper.totalLikes(listWithNoBlogs);
+    assert.strictEqual(result, 0);
+  });
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  // if (blogs.length === 1) {
+  //   return blogs[0].likes;
+  // }
+
+  return blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0
+  );
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
