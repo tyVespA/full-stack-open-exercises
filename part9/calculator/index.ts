c78d4cb3803d66ddf204c65f56d2cfbbcc74f61f
tyVespA <migliavacca.marco08@gmail.com>
2025-03-21 17:14:34 +0100

    complete 9.4

---
+++ part9/calculator/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("App running on port", PORT);
+});
