9603e2267ff2deec8e8da4cc898bf98a2d9fea71
tyVespA <migliavacca.marco08@gmail.com>
2025-03-21 17:53:08 +0100

    complete 9.5

--- part9/calculator/index.ts
+++ part9/calculator/index.ts
@@ -1,11 +1,33 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
-const PORT = 3001;
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.json({ error: "malformatted parameters" });
+    return;
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  const bmiCalculation = {
+    weight,
+    height,
+    bmi,
+  };
+
+  res.json(bmiCalculation);
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
   console.log("App running on port", PORT);
 });
